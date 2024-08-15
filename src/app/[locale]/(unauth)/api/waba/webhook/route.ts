b9afbef5f07a37f5055046b45a59f327fd21c0ce
xogz3 <saravanangokult@gmail.com>
2024-08-15 14:57:05 +0530

    fix: change next req to next api req

--- src/app/[locale]/(unauth)/api/waba/webhook/route.ts
+++ src/app/[locale]/(unauth)/api/waba/webhook/route.ts
@@ -2,6 +2,7 @@
 
 import crypto from 'crypto';
 import { getFirestore } from 'firebase-admin/firestore';
+import type { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
 
 import { replyToUser } from '@/utils/ReplyHelper';
@@ -45,12 +46,10 @@ function parseMessagePayload(payload: any) {
   };
 }
 
-export async function GET(request: Request) {
-  console.log('GET FUNCTION');
-  const url = new URL(request.url);
-  const mode = url.searchParams.get('hub.mode');
-  const token = url.searchParams.get('hub.verify_token');
-  const challenge = url.searchParams.get('hub.challenge');
+export async function GET(request: NextApiRequest) {
+  const mode = request.query['hub.mode'];
+  const token = request.query['hub.verify_token'];
+  const challenge = request.query['hub.challenge'];
   if (mode === 'subscribe' && token === process.env.WEBHOOK_VERIFY_TOKEN) {
     return NextResponse.json(challenge, { status: 200 });
   }

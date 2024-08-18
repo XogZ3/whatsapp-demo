be7884e2553321e95bd2ccc2d6dc7500ec6e01dd
xogz3 <saravanangokult@gmail.com>
2024-08-18 19:40:42 +0530

    fix: try old route

--- src/app/api/waba/webhook/route.ts
+++ src/app/api/waba/webhook/route.ts
@@ -29,72 +29,6 @@ export async function GET(request: Request) {
   return new Response(JSON.stringify('Invalid verify_token'), { status: 403 });
 }
 
-async function processWebhook(body: any) {
-  try {
-    console.log('Processing webhook');
-    const data = JSON.parse(body);
-    console.log('Parsed body:', data);
-
-    const subscriptionObject = data.object;
-    const wabaId = data.entry[0].id;
-    const { field, value: payload } = data.entry[0].changes[0];
-
-    if (
-      subscriptionObject === 'whatsapp_business_account' &&
-      field === 'messages' &&
-      wabaId === process.env.WABA_ID
-    ) {
-      const messageObject = parseMessagePayload(payload);
-
-      if (
-        messageObject.clientid &&
-        messageObject.timestamp &&
-        (messageObject.type === 'message' || messageObject.type === 'status')
-      ) {
-        const clientDoc = firestore
-          .collection('apps')
-          .doc(wabaId as string)
-          .collection('clients')
-          .doc(messageObject.clientid);
-
-        if (messageObject.type === 'message') {
-          await clientDoc.collection('messages').add(messageObject);
-        }
-
-        const updates: any = { lastupdatedat: messageObject.timestamp };
-        if (messageObject.status_raw?.conversation?.expiration_timestamp) {
-          updates.whatsappExpiration =
-            messageObject.status_raw?.conversation?.expiration_timestamp;
-        }
-        if (messageObject.type === 'message') {
-          updates.lastseen = messageObject.timestamp;
-          if (messageObject.contact?.profile?.name) {
-            updates.name = messageObject.contact?.profile?.name;
-          }
-        }
-
-        await clientDoc.set(updates, { merge: true });
-
-        console.log(
-          'webhook triggered on: ',
-          messageObject.type,
-          messageObject.timestamp,
-          messageObject.type === 'message' ? messageObject.message : '',
-        );
-
-        if (
-          messageObject.type === 'message' &&
-          messageObject.timestamp > Date.now() / 1000 - 180
-        ) {
-          await replyToUser(messageObject);
-        }
-      }
-    }
-  } catch (e) {
-    console.error('Error in processWebhook:', e);
-  }
-}
-
 export async function POST(request: Request) {
   const body = await request.text();
   const signature = request.headers.get('x-hub-signature')?.toString();
@@ -106,26 +40,73 @@ export async function POST(request: Request) {
   );
   hmac.update(body, 'ascii');
   const expectedSignature = `sha1=${hmac.digest('hex')}`;
-  console.log(
-    'expectedSignature',
-    expectedSignature,
-    signature === expectedSignature,
-  );
-
-  // Immediately return a success response to Facebook
-  const immediateResponse = new Response('success', {
-    status: 200,
-    headers: corsHeaders,
-  });
+  console.log('expectedSignature', expectedSignature);
 
   if (signature === expectedSignature) {
-    // Process the webhook data in the background
-    processWebhook(body).catch((e) => {
-      console.error('Error processing webhook:', e);
-    });
-  } else {
-    console.error('Invalid signature');
-  }
+    try {
+      const data = JSON.parse(body);
+      const subscriptionObject = data.object;
+      const wabaId = data.entry[0].id;
+      const { field, value: payload } = data.entry[0].changes[0];
 
-  return immediateResponse; // Return immediately after verifying the signature
+      if (
+        subscriptionObject === 'whatsapp_business_account' &&
+        field === 'messages' &&
+        wabaId === process.env.WABA_ID
+      ) {
+        const messageObject = parseMessagePayload(payload);
+
+        if (
+          messageObject.clientid &&
+          messageObject.timestamp &&
+          (messageObject.type === 'message' || messageObject.type === 'status')
+        ) {
+          const clientDoc = firestore
+            .collection('apps')
+            .doc(wabaId as string)
+            .collection('clients')
+            .doc(messageObject.clientid);
+
+          if (messageObject.type === 'message') {
+            await clientDoc.collection('messages').add(messageObject);
+          }
+
+          const updates: any = { lastupdatedat: messageObject.timestamp };
+          if (messageObject.status_raw?.conversation?.expiration_timestamp) {
+            updates.whatsappExpiration =
+              messageObject.status_raw?.conversation?.expiration_timestamp;
+          }
+          if (messageObject.type === 'message') {
+            updates.lastseen = messageObject.timestamp;
+            if (messageObject.contact?.profile?.name) {
+              updates.name = messageObject.contact?.profile?.name;
+            }
+          }
+
+          await clientDoc.set(updates, { merge: true });
+
+          console.log(
+            'webhook triggered on: ',
+            messageObject.type,
+            messageObject.timestamp,
+            messageObject.type === 'message' ? messageObject.message : '',
+          );
+
+          if (
+            messageObject.type === 'message' &&
+            messageObject.timestamp > Date.now() / 1000 - 180
+          ) {
+            await replyToUser(messageObject);
+          }
+        }
+      }
+
+      return new Response('success', { status: 200, headers: corsHeaders });
+    } catch (e) {
+      console.error(e);
+      return new Response('unable to save response', { status: 200 });
+    }
+  } else {
+    return new Response('Invalid signature', { status: 401 });
+  }
 }

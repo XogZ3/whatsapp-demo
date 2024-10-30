e44180f0ba9bebfe7e07a90c3cc814f189341ed3
xogz3 <saravanangokult@gmail.com>
2024-10-30 20:40:49 +0530

    fix: canc sub page metadata

---
+++ src/app/[locale]/(unauth)/cancel-subscription/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+
+import { getBaseUrl } from '@/utils/helpers';
+
+export async function generateMetadata(props: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const t = await getTranslations({
+    locale: props.params.locale,
+    namespace: 'CancelSubscription',
+  });
+  const baseUrl = getBaseUrl();
+
+  return {
+    metadataBase: new URL(baseUrl),
+    title: t('meta_title') as string,
+    description: t('meta_description') as string,
+    twitter: {
+      card: 'summary_large_image',
+    },
+    openGraph: {
+      title: t('meta_title') as string,
+      description: t('meta_description') as string,
+    },
+    alternates: {
+      canonical: `${baseUrl}/cancel-subscription`,
+      languages: {
+        pt: `${baseUrl}/pt/cancel-subscription`,
+      },
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+}
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
